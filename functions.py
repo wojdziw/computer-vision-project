cd953a2c4c8196d4999a32e788abb05b27f80e87
adam <adam.alsegard@gmail.com>
2016-10-20 13:46:19 +0800

    basic sift works

--- functions.py
+++ functions.py
@@ -1,254 +1,50 @@
-import cv2
 import numpy as np
+import cv2
+import math
 
-def MyConvolve(image, ff):
-	result = np.zeros(image.shape)
-	width = image.shape[0]
-	height = image.shape[1]
 
-	# Flip the filter
-	ff = np.transpose(np.transpose(ff[::-1])[::-1])
 
-	kernelSize = ff.shape[0]
-	margin = kernelSize/2
-
-	# Create a bigger matrix to pad with zeroes
-	biggerResult = np.zeros([width+margin*2,height+margin*2])
-	biggerResult[1:width+margin,1:height+margin] = image
-
-	# Convolve
-	for i in range(1,width+margin):
-		for j in range(1,height+margin):
-			multiplied = np.multiply(biggerResult[i-margin:i+margin+1,j-margin:j+margin+1],ff)
-			summed = np.sum(multiplied)
-			result[i-margin,j-margin] = summed
-
-	return result
-
-def nms(image):
-	result = np.zeros(image.shape)
-	edges = sobel(image)
-
-	width = image.shape[0]
-	height = image.shape[1]
-
-	for i in range(width):
-		for j in range(height):
-			this = edges[i,j]
-
-			# Save the neigbour values, make sure they are within the matrix (hence the mins and maxs)
-			top = edges[max(i-1,0),j]
-			bottom = edges[min(i+1,width-1),j]
-			left = edges[i,max(j-1,0)]
-			right = edges[i,min(j+1,height-1)]
-
-			# Retain the edge if it's maximum in either axis
-			if (max(top,bottom,this)==this) or (max(left,right,this)==this):
-				result[i,j] = this
-
-	return result
-
-def sobel(image):
-	result = np.zeros(image.shape)
-	horizontal = np.zeros(image.shape)
-	vertical = np.zeros(image.shape)
-
-	horizontalFilter = np.matrix([[-1,0,1],[-2,0,2],[-1,0, 1]])
-	verticalFilter = np.matrix([[1,2,1],[0,0,0],[-1,-2,-1]])
-
-	# Find the horizontal and vertical edges first
-	horizontal = MyConvolve(image, horizontalFilter)
-	vertical = MyConvolve(image, verticalFilter)
-
-	# Combine the two to get all edges
-	result = np.sqrt(horizontal*horizontal+vertical*vertical)/4
-
-	return result
-
-def sobel_hor(image):
-	result = np.zeros(image.shape)
-	horizontal = np.zeros(image.shape)
-
-	horizontalFilter = np.matrix([[1,2,1],[0,0,0],[-1,-2,-1]])
-
-	# Find the horizontal edges first
-	horizontal = MyConvolve(image, horizontalFilter)
-
-	# Combine the two to get all edges
-	result = horizontal/4
-
-	return result
-
-def sobel_ver(image):
-	result = np.zeros(image.shape)
-	vertical = np.zeros(image.shape)
-
-	verticalFilter = np.matrix([[-1,0,1],[-2,0,2],[-1,0, 1]])
-
-	# Find the vertical edges first
-	vertical = MyConvolve(image, verticalFilter)
-
-	# Combine the two to get all edges
-	result = vertical/4
-
-	return result
-
-def gauss_kernels(size,sigma=1.0):
+def gauss_kernels(size, sigma=1.0):
 	## returns a 2d gaussian kernel
 	if size<3:
 		size = 3
 	m = size/2
+
 	x, y = np.mgrid[-m:m+1, -m:m+1]
 	kernel = np.exp(-(x*x + y*y)/(2*sigma*sigma))
 	kernel_sum = kernel.sum()
+
 	if not sum==0:
 		kernel = kernel/kernel_sum
+
 	return kernel
 
-def corner_detector(image, imageRGB):
 
-	gx = sobel_hor(image)
-	gy = sobel_ver(image)
+def MyConvolve (img, kernel):
+	result = np.zeros(img.shape)
+	rows = img.shape[0]
+	cols = img.shape[1]
 
-	Ixx = gx*gx
-	Ixy = gx*gy
-	Iyy = gy*gy
+	margin = kernel.shape[0]/2
 
-	kernel = gauss_kernels(3,1)
+	# zero padding
+	padImg = np.zeros((rows+2*margin, cols+2*margin))
+	padImg[margin:rows+margin, margin:cols+margin] = img
 
-	Wxx = MyConvolve(Ixx, kernel)
-	Wxy = MyConvolve(Ixy, kernel)
-	Wyy = MyConvolve(Iyy, kernel)
-
-	k = 0.06
-	threshold = 0.1
-	stepsize = 10
-
-	responses = np.zeros(Wxx.shape)
-
-	for i in range(0,Wxx.shape[0],stepsize):
-		for j in range(0,Wxx.shape[1],stepsize):
-			W = np.zeros([2,2])
-			W[0,0] = Wxx[i,j]
-			W[0,1] = Wxy[i,j]
-			W[1,0] = Wxy[i,j]
-			W[1,1] = Wyy[i,j]
-
-			detW = W[0,0]*W[1,1]-W[0,1]*W[1,0]
-			traceW = W[0,0]+W[1,1]
-
-			response = detW-k*traceW*traceW
-
-			responses[i,j] = response
-
-	maxVal = np.max(responses)
-
-	for i in range(4,Wxx.shape[0]-4):
-		for j in range(4,Wxx.shape[1]-4):
-			if responses[i,j] >= maxVal*threshold:
-				imageRGB[i-4:i+4,j-4,0] = 0
-				imageRGB[i-4:i+4,j-4,1] = 0
-				imageRGB[i-4:i+4,j-4,2] = 255
-
-				imageRGB[i-4:i+4,j+4,0] = 0
-				imageRGB[i-4:i+4,j+4,1] = 0
-				imageRGB[i-4:i+4,j+4,2] = 255
-
-				imageRGB[i-4,j-4:j+4,0] = 0
-				imageRGB[i-4,j-4:j+4,1] = 0
-				imageRGB[i-4,j-4:j+4,2] = 255
-
-				imageRGB[i+4,j-4:j+5,0] = 0
-				imageRGB[i+4,j-4:j+5,1] = 0
-				imageRGB[i+4,j-4:j+5,2] = 255
-
-
-
-	return imageRGB
-
-def componentCoords(image,indicatedLocation):
-	# Cropping out a window around the indicated component to find its colour
-	sample = image[indicatedLocation[0]-5:indicatedLocation[0]+5,indicatedLocation[1]-5:indicatedLocation[1]+5]
-	greenAvg = np.mean(sample[:,:,0])
-	blueAvg = np.mean(sample[:,:,1])
-	redAvg = np.mean(sample[:,:,2])
-	# finding the average colour of the component
-	sampleColour = [greenAvg,blueAvg,redAvg]
-
-	# traversing
-	visited = np.zeros([image.shape[0], image.shape[1]], dtype=bool)
-	notSameColour = np.zeros([image.shape[0], image.shape[1]], dtype=bool)
-
-	sumX = 0
-	sumY = 0
-	total = 0.0
-	visited, notSameColour, sumX, sumY, total = traverseOut(image, indicatedLocation[0], indicatedLocation[1], sampleColour, visited, notSameColour, sumX, sumY, total)
-
-	bottomestX = 0
-	bottomestY = 0
-
-	for i in range(image.shape[0]):
-		for j in range(image.shape[1]):
-			if notSameColour[i,j]:
-				if i > bottomestX:
-					bottomestX = i
-					bottomestY = j
-
-
-	centreX = sumX/total
-	centreY = sumY/total
-
-	return centreX, centreY, bottomestX, bottomestY, notSameColour, visited
+	# Flip kernel!
+	kernel = np.fliplr(np.flipud(kernel))
 	
-def traverseOut(image,i,j,sampleColour,visited, notSameColour, sumX, sumY, total):
-	tolerance = 0.2
+	# Skip the zero padding so we don't detect the edges later on
+	for i in range(margin, rows+margin):
+		for j in range(margin, cols+margin):
+			
+			#value = (padImg[i-1,j-1]*kernel[0,0])+(padImg[i-1,j]*kernel[0,1])+(padImg[i-1,j+1]*kernel[0,2])+ \
+			#		(padImg[i,j-1]*kernel[1,0])+(padImg[i,j]*kernel[1,1])+(padImg[i,j+1]*kernel[1,2])+ \
+			#		(padImg[i+1,j-1]*kernel[2,0])+(padImg[i+1,j]*kernel[2,1])+(padImg[i+1,j+1]*kernel[2,2])
+			
+			Mvalue = np.multiply(padImg[i-margin:i+margin+1,j-margin:j+margin+1],kernel)
+			value = np.sum(Mvalue)
 
-	visited[i,j] = True
-	sameColour = True
+			result[i-margin, j-margin] = abs(value)
 
-	sampleGreen = sampleColour[0]
-	sampleRed = sampleColour[1]
-	sampleBlue = sampleColour[2]
-
-	green = image[i,j,0]
-	red = image[i,j,1]
-	blue = image[i,j,2]
-
-	# Check if the investigated coordinate is of the same colour
-	if green < sampleGreen*(1-tolerance) or green > sampleGreen*(1+tolerance):
-		sameColour = False
-
-	if red < sampleRed*(1-tolerance) or red > sampleRed*(1+tolerance):
-		sameColour = False
-
-	if blue < sampleBlue*(1-tolerance) or blue > sampleBlue*(1+tolerance):
-		sameColour = False
-
-	if not sameColour:
-		sumX += i
-		sumY += j
-		total += 1
-		notSameColour[i,j] = True
-		return visited, notSameColour, sumX, sumY, total
-
-	# If we are still within the same sample, traverse out further
-	if sameColour:
-		if not visited[i-1,j-1]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i-1,j-1,sampleColour,visited, notSameColour, sumX, sumY, total)
-		if not visited[i-1,j]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i-1,j,sampleColour,visited, notSameColour, sumX, sumY, total)
-		if not visited[i-1,j+1]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i-1,j+1,sampleColour,visited, notSameColour, sumX, sumY, total)
-		if not visited[i,j+1]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i,j+1,sampleColour,visited, notSameColour, sumX, sumY, total)
-		if not visited[i+1,j+1]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i+1,j+1,sampleColour,visited, notSameColour, sumX, sumY, total)
-		if not visited[i+1,j]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i+1,j,sampleColour,visited, notSameColour, sumX, sumY, total)
-		if not visited[i+1,j-1]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i+1,j-1,sampleColour,visited, notSameColour, sumX, sumY, total)
-		if not visited[i,j-1]:
-			visited, notSameColour, sumX, sumY, total = traverseOut(image,i,j-1,sampleColour,visited, notSameColour, sumX, sumY, total)
-
-	return visited, notSameColour, sumX, sumY, total
-
+	return result
--- main.py
+++ main.py
@@ -4,16 +4,24 @@ import numpy as np
 import cv2
 import math
 import sys
+from functions import *
 
 # Choose which video to process 
-try:
-	vidNr = str(sys.argv[1])
-except:
+if (sys.argv < 1):
 	print 'Usage: main.py <VideoIndex>'
+else:
+	try:
+		vidNr = str(sys.argv[1])
+	except:
+		print 'Usage: main.py <VideoIndex>'
 
-indLoc = np.zeros(2, np.uint8)
-indLoc[0] = 227 #uint8(argv[2])
-indLoc[1] = 494 #uint8(argv[3])
+# Where to start look
+indLoc = np.zeros(2, int)
+indLoc[0] = 235 #uint8(argv[2])
+indLoc[1] = 496 #uint8(argv[3])
+rad = 10
+
+print indLoc
 
 vidObj = cv2.VideoCapture('vids/beachVolleyball'+vidNr+'.mov')
 
@@ -24,53 +32,104 @@ frCount = int(vidObj.get(cv2.CAP_PROP_FRAME_COUNT))
 
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
-outObj = cv2.VideoWriter('output/vid'+vidNr+'2.avi',fourcc, frFPS, (frWidth, frHeight))
+outObj = cv2.VideoWriter('output/vid'+vidNr+'3.avi',fourcc, frFPS, (frWidth, frHeight))
 
 # Read first frame
 _,img = vidObj.read()
 outImg = np.zeros(img.shape, np.uint8)
 #cv2.imwrite('vid1.jpg', img)
 
-# Define the region mask (25x25) where to look
+
+# Define the region mask where to look
 mask = np.zeros(img.shape[:2], np.uint8)
-mask[indLoc[0]-24:indLoc[0]+24,indLoc[1]-24:indLoc[1]+24] = 255
-#mask[:,:] = 2
+mask[indLoc[0]-rad:indLoc[0]+rad,indLoc[1]-rad:indLoc[1]+rad] = 255
+cv2.imwrite('mask.jpg', mask)
+
+test = np.zeros(img.shape, np.uint8)
+test[indLoc[0]-rad:indLoc[0]+rad,indLoc[1]-rad:indLoc[1]+rad] = img[indLoc[0]-rad:indLoc[0]+rad,indLoc[1]-rad:indLoc[1]+rad]
+cv2.imwrite('test.jpg', test)
+#print mask.shape
+
+# Use gaussian blur
+#grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#gauss = gauss_kernels(5,1)
+#img = MyConvolve(grayImg, gauss)
+#cv2.imwrite('img.jpg', img)
+
+
 
 # Create SIFT and find the best features in the first frame
 # Could probably use our own detector (Harris/Tomasi) and just use this as compute descriptor
 # We would need to have the input as cv.KeyPoints in that case! 
-#grayImg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 sift = cv2.xfeatures2d.SIFT_create()				# Look for features
 (kp1, desc1) = sift.detectAndCompute(img, mask)		# Look only in mask 
 
+# Convert to 2D point and find the bottom-most keypoint!
+pts = np.zeros([len(kp1), 2])
+btm, foot = 0, 0
+firstPt = np.zeros(2, int)
+
+for idx in range(len(kp1)):
+	pts[idx] = kp1[idx].pt # [x, y]
+	if (pts[idx][1]>btm):
+		foot = idx
+		btm = pts[idx][1]
+
+desc1 = np.matrix(desc1[foot])
+firstPt = [int(pts[foot][1]), int(pts[foot][0])] #change [x, y] to [row, col] 
+print desc1
+print firstPt
+print len(kp1)
+print len(desc1)
+
+# Draw first frame
+
+outImg = img
+#cv2.drawKeypoints(img, kp1, outImg, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+mask[firstPt[0]-5:firstPt[0]+5, firstPt[1]-5:firstPt[1]+5] = 255 
+mask[firstPt[0]-3:firstPt[0]+3, firstPt[1]-3:firstPt[1]+3] = 0
+outImg[mask] = [255, 0, 0] 
+outObj.write(outImg)
 
 for fr in range(1, frCount):
 
 	_,nextFr = vidObj.read()
 
-	(kp2, descs2) = sift.detectAndCompute(nextFr, None)
+	# Use gaussian blur
+	#grayImg = cv2.cvtColor(nextFr, cv2.COLOR_BGR2GRAY)
+	#gauss = gauss_kernels(5,1)
+	#nextFr = MyConvolve(grayImg, gauss)
 
-	# Find matches
-	FLANN_INDEX_KDTREE = 0
-   	index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-   	search_params = dict(checks = 50)
-   	
-	flann = cv2.FlannBasedMatcher(index_params, search_params)
-	 
-	matches = flann.knnMatch(des1,des2,k=2)
-	 
-	# store all the good matches as per Lowe's ratio test.
-	good = []
-	for m,n in matches:
-		if m.distance < 0.7*n.distance:
-			good.append(m)
+	# Find all desciptors in image, should use bigger mask only!?
+	mask[firstPt[0]-2*rad:firstPt[0]+2*rad,firstPt[1]-2*rad:firstPt[1]+2*rad] = 255
+	(kp2, desc2) = sift.detectAndCompute(nextFr, mask)
 
 
+	# Define Brute Force matcher (Find the nearest descriptor)
+	bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
+	matches = bf.match(desc1, desc2) # Returns best result for the foot index
 
-	cv2.drawKeypoints(nextFr, kp2, outImg, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+	# DMatch objects, (have distance, trainIdx, queryIdx (index of descriptors), imgIdx) 
+	matches = sorted(matches, key = lambda x:x.distance)
+	print len(matches)
 
+	if(len(matches)>=1):
+		newPt = kp2[matches[0].trainIdx].pt
+		newPt = [int(newPt[1]), int(newPt[0])]
+	
+	#desc1 = desc2[matches[0].queryIdx]
+	desc1 = desc2
+	firstPt = newPt
+
+	print newPt
+
+	# Draw first frame
+	outImg = nextFr
+	#cv2.drawKeypoints(nextFr, kp2, outImg, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+	mask[firstPt[0]-5:firstPt[0]+5, firstPt[1]-5:firstPt[1]+5] = 255 
+	mask[firstPt[0]-3:firstPt[0]+3, firstPt[1]-3:firstPt[1]+3] = 0
+	outImg[mask] = [255, 0, 0] 
 	outObj.write(outImg)
-
 
 vidObj.release()
 outObj.release()
