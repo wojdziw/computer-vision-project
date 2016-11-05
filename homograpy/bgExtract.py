6238994be7b55f01ef8868249ddb5c2cc3f06b8f
YasenPetrov <yasenpetr@gmail.com>
2016-11-05 12:00:30 +0800

    Point position smoothing

--- homograpy/bgExtract.py
+++ homograpy/bgExtract.py
@@ -1,7 +1,75 @@
 import numpy as np
+import numpy
 import cv2
 import cv2.cv as cv
 import sys
+
+
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=numpy.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    return y
+
+
+def sm2d(bl, ks=11):
+    bls0 = smooth(bl[:, 0], window_len = ks)
+    bls1 = smooth(bl[:, 1], window_len = ks)
+    smbl = np.empty([bls0.shape[0], 2])
+    smbl[:, 0] = bls0
+    smbl[:, 1] = bls1
+    return smbl
 
 
 if len(sys.argv) < 2:
@@ -9,10 +77,26 @@ if len(sys.argv) < 2:
     print "Usage: bgExctract.py <path_to_video_file> [seconds_start, default -> start of video] [seconds_end, default -> end of video]"
     sys.exit(1)
 
+
 bl = np.load('./data/coords_video1/1st_coords.npy')
 br = np.load('./data/coords_video1/2nd_coords.npy')
 pl = np.load('./data/coords_video1/3rd_coords.npy')
 pr = np.load('./data/coords_video1/4th_coords.npy')
+bl = bl[:-20, :]
+br = br[:-20, :]
+pl = pl[:-20, :]
+pr = pr[:-20, :]
+l = bl.shape[0]
+
+ks = 19
+bl = sm2d(bl, ks)
+br = sm2d(br, ks)
+pl = sm2d(pl, ks)
+pr = sm2d(pr, ks)
+bl = bl[ks / 2: - ks / 2, :]
+br = br[ks / 2: - ks / 2, :]
+pl = pl[ks / 2: - ks / 2, :]
+pr = pr[ks / 2: - ks / 2, :]
 
 filename = sys.argv[1]
 seconds_start = 0
@@ -48,9 +132,9 @@ prevPts = [bl[frame_start], br[frame_start], pl[frame_start], pr[frame_start]]
 firstPts = [bl[frame_start], br[frame_start], pl[frame_start], pr[frame_start]]
 ret, firstFrame = cap.read()
 avgImg = np.float32(firstFrame)
-frame_index = frame_start + 1
+frame_index = frame_start
 frame_count = 1
-while(ret and frame_index < frames_end):
+while(ret and frame_index < frames_end and frame_index < bl.shape[0]):
     if paused == False:
         # print frame_index, cap.get(cv.CV_CAP_PROP_POS_FRAMES)
         # print cap.get(cv.CV_CAP_PROP_POS_MSEC)
@@ -80,7 +164,6 @@ while(ret and frame_index < frames_end):
         # Use the pixels from the previous iteration of averaging for the parts
         # of the frame which are not covered by the warped image
         avgImg = black * prevAvg + (1 - black) * avgImg
-
 
         frame = np.concatenate((frame, avgImg, warpedFrame), axis = 0)
         cv2.imshow('warp', cv2.convertScaleAbs(frame))
--- homograpy/bgSubstract.py
+++ homograpy/bgSubstract.py
@@ -1,7 +1,75 @@
 import numpy as np
+import numpy
 import cv2
 import cv2.cv as cv
 import sys
+
+
+def smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+
+    input:
+        x: the input signal
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+
+    see also:
+
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        raise ValueError, "Input vector needs to be bigger than window size."
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=numpy.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=numpy.ones(window_len,'d')
+    else:
+        w=eval('numpy.'+window+'(window_len)')
+
+    y=numpy.convolve(w/w.sum(),s,mode='valid')
+    return y
+
+
+def sm2d(bl, ks=11):
+    bls0 = smooth(bl[:, 0], window_len = ks)
+    bls1 = smooth(bl[:, 1], window_len = ks)
+    smbl = np.empty([bls0.shape[0], 2])
+    smbl[:, 0] = bls0
+    smbl[:, 1] = bls1
+    return smbl
 
 
 if len(sys.argv) < 3:
@@ -14,6 +82,23 @@ bl = np.load('./data/coords_video1/1st_coords.npy')
 br = np.load('./data/coords_video1/2nd_coords.npy')
 pl = np.load('./data/coords_video1/3rd_coords.npy')
 pr = np.load('./data/coords_video1/4th_coords.npy')
+
+bl = bl[:-20, :]
+br = br[:-20, :]
+pl = pl[:-20, :]
+pr = pr[:-20, :]
+l = bl.shape[0]
+
+ks = 19
+bl = sm2d(bl, ks)
+br = sm2d(br, ks)
+pl = sm2d(pl, ks)
+pr = sm2d(pr, ks)
+bl = bl[ks / 2: - ks / 2, :]
+br = br[ks / 2: - ks / 2, :]
+pl = pl[ks / 2: - ks / 2, :]
+pr = pr[ks / 2: - ks / 2, :]
+
 
 filename = sys.argv[1]
 background_file = sys.argv[2]
